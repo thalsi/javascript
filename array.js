b7e7bca5f3e320ada23c07498eae7cad0ec67588
thalsi <thalseehthalsi609@gmail.com>
2021-07-20 23:57:03 +0530

    array

---
+++ array.js
@@ -0,0 +1,34 @@
+/*
+
+An array is a data structure that contains a group of elements.
+
+*/
+
+
+userId=[10, 20, 30, 40, 50, 60, 70];
+userName=['ali', 'aju', 'ajumal', 'shammer', 'shabari'];
+
+//array length
+console.log("Array length = "+userId.length);
+
+//Array index postion
+
+console.log("index postion = "+userName[2]);
+console.log("index postion = "+ userName[userName.length-2]);
+
+//loop of array
+//arry loop are main three type
+//1. for loop
+//2.forEach
+//3.for..of
+
+//for loop
+for (let index = 0; index < userName.length; index++) {
+    console.log("for loop ="+userName[index]);
+}
+
+//forEach
+userName.forEach((element, index) => {
+    console.log("ForEach =>"+ element +" index=>"+index);
+});
+
